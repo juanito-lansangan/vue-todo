63aeff280ebecc6ba18faf602358ecaafa9a7851
jplans <juanito.lansangan@gmail.com>
2017-10-01 23:06:30 +0800

    update to vue component base

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Todo from '@/components/Todo'
 import TodoApp from '@/components/TodoApp'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    //   {
-    //       path: '/',
-    //       name: 'todo',
-    //       component: Todo
-    //   },
       {
           path: '/',
           name: 'todoapp',
